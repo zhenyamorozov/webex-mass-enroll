3052a63e3159302278449c872adb45c004eb7d87
emorozov <zhenya.morozov@gmail.com>
2022-11-18 13:49:46 +0600

    simplify and streamline

--- massenroll.py
+++ massenroll.py
@@ -1,4 +1,4 @@
-"""Simple but useful script to mass enroll users in to Webex space.
+"""Simple but useful script to mass enroll users into a Webex space.
 Set your accessToken, roomId, and provide a list of emails to enroll.
 """
 
@@ -6,35 +6,32 @@ accessToken = "your access token"
 roomId = "your room ID"
 
 emails = [
-"user1@example.com",
-"user2@example.com",
+    "user1@example.com",
+    "user2@example.com",
 ]
 
-import json
+
 import requests
 
-requests.packages.urllib3.disable_warnings()
+headers = {
+    'Authorization': "Bearer " + accessToken,
+    'Content-Type': "application/json; charset=utf-8"
+}
 
-headers = {"Authorization": "Bearer " + accessToken,
-           "Content-Type": "application/json; charset=utf-8"}
-
-i = 1
+i = 0
 
 for email in emails:
-    body_json = {
-        "roomId" : roomId,
-        "personEmail" : email,
-    }
-
-    resp = requests.post("https://api.ciscospark.com/v1/memberships",
-                    json.dumps(body_json), headers=headers, verify=False)
-
-    status = resp.status_code
-    #print(resp.json())
-    if "message" in resp.json():
-        message = resp.json()["message"]
-    else:
-        message = ""
-    print(str(i)+" Email: "+email+" Status: "+str(status)+" "+message+"\n")
     i+=1
 
+    body = {
+        'roomId' : roomId,
+        'personEmail' : email,
+    }
+
+    resp = requests.post(
+        "https://webexapis.com/v1/memberships", 
+        json = body, 
+        headers = headers
+    )
+
+    print("{} Email: {} Status: {} {}".format(i, email, resp.status_code, resp.json()["message"] if "message" in resp.json() else ""))
